a7db705ab6a3aceb6580d09582afd828fabf231a
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-11-01 13:51:09 +0100

    z
    z

---
+++ commands/serverinfo.js
@@ -0,0 +1,31 @@
+const discord = require("discord.js");
+const { size } = require("mathjs");
+const moment = require("moment");
+
+module.exports.run = async (client, message, args) => {
+
+    var roles = message.guild.roles.cache.size - 1;
+
+    var embed = new discord.MessageEmbed()
+        .setColor("#0070fa")
+        .setThumbnail(`${message.guild.iconURL({ size: 4096 })}`)
+        .setTitle(`${message.guild.name}`)
+        .addField("ID:", `${message.guild.id}`)
+        .addField("Eigenaar:", `${message.guild.owner.user.tag}`, true)
+        .addField("Regio:", `${message.guild.region}`, true)
+        .addField("Bots:", `${message.guild.members.cache.filter(m => m.user.bot).size}`, true)
+        .addField("Mensen:", `${message.guild.memberCount - message.guild.members.cache.filter(m => m.user.bot).size}`, true)
+        .addField("Totaal aantal leden:", `${message.guild.memberCount}`, true)
+        .addField("Online:", `${message.guild.members.cache.filter(m => m.user.presence.status == "online" || m.user.presence.status == "dnd" || m.user.presence.status == "idle").size}`, true)
+        .addField("Tekstkanalen:", `${message.guild.channels.cache.filter(chan => chan.type == "text").size}`, true)
+        .addField("Spraakkanalen:", `${message.guild.channels.cache.filter(chan => chan.type == "voice").size}`, true)
+        .addField("Server gemaakt op:", `${moment(message.guild.createdAt).format("LL")}`)
+        .addField("Server gejoined:", `${moment(message.member.joinedAt).format("LL")}`)
+        .addField(`Rollen [${roles}]`, `${message.guild.roles.cache.map(r => r).join(" ")}`);
+    
+    return message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "serverinfo"
+}
---
+++ commands/suggestie.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+module.exports.run = async (Client, message, args) => {
+
+    //message.delete();
+
+
+
+    var text = args || `Fout.`;
+
+    let suggestie = args.join(" ").slice(10);
+
+    var suggestieKanaal = message.member.guild.channels.cache.get("769303214188396584");
+
+    if (!suggestieKanaal) return message.channel.send("Kan de suggestie channel niet vinden.")
+
+
+    const embed = new discord.MessageEmbed()
+        .setDescription(`${message.author.username} ${message.user.displayAvatarURL({size: 500})}`)
+        .setColor("#15214d")
+        .addField("Suggestie", suggestie)
+
+
+
+    message.channel.send("✅ uw Suggestie is goed verzonden naar het Suggestie kanaal");
+    var suggestieKanaal = message.guild.channels.cache.find(ch => ch.name === "suggesties");
+    if (!suggestieKanaal) return message.guild.send("Kan het kanaal niet vinden");
+
+    suggestieKanaal.send(embed).then(embedMessage => {
+        embedMessage.react('✅');
+        embedMessage.react('😐');
+        embedMessage.react('❌');
+
+    });
+
+}
+
+module.exports.help = {
+    name: "suggestie"
+}
