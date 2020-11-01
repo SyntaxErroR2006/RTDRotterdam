194f2d063980416df66f0881158e905bd998a779
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-11-01 13:58:59 +0100

    Update suggestie.js

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -1,38 +1,37 @@
 const discord = require("discord.js");
 
-module.exports.run = async (Client, message, args) => {
+module.exports.run = async(Client, message, args) =>{
 
-    //message.delete();
+     //message.delete();
 
 
-
+   
     var text = args || `Fout.`;
 
-    let suggestie = args.join(" ").slice(10);
+    let suggestie = args.join(" ").slice(8);
 
-    var suggestieKanaal = message.member.guild.channels.cache.get("758349088588300293");
+    var suggestieKanaal = message.member.guild.channels.cache.get("769303214188396584");
 
-    if (!suggestieKanaal) return message.channel.send("Kan de suggestie channel niet vinden.")
+    if(!suggestieKanaal) return message.channel.send("Kan de suggestie channel niet vinden.")
 
 
     const embed = new discord.MessageEmbed()
-        .setDescription(`${message.author.username}`)
+        .setTitle(`${message.author.username} heeft een suggestie verstuurd `)
         .setColor("#15214d")
         .addField("Suggestie", suggestie)
-
+        .setThumbnail(message.author.displayAvatarURL())
+    
 
 
     message.channel.send("✅ uw Suggestie is goed verzonden naar het Suggestie kanaal");
     var suggestieKanaal = message.guild.channels.cache.find(ch => ch.name === "suggesties");
     if (!suggestieKanaal) return message.guild.send("Kan het kanaal niet vinden");
-
+ 
     suggestieKanaal.send(embed).then(embedMessage => {
         embedMessage.react('✅');
-        embedMessage.react('😐');
         embedMessage.react('❌');
-
     });
-
+ 
 }
 
 module.exports.help = {
