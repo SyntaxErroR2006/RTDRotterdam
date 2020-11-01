1f74b47347530f6833847ab1dd94ba36563d41a0
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-11-01 21:14:53 +0100

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     var userName = message.author.username;
     var userDiscriminator = message.author.discriminator;
 
-    var channel = createdChannel;
+    var channel = message.guild.channels(userName.toLowerCase() + "-" + "ticket", { type: "text" });
 
     var ticketBestaat = false;
 
