b5b0fb76120fa570e6baa78a3ff717d957435bab
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-11-01 21:34:07 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,15 @@
-const discord = require("discord.js");  
-const { re } = require("mathjs");
+const discord = require("discord.js"); 
 
 module.exports.run = async(client, message, args) => {
 
     var embed = new discord.MessageEmbed()
     .setTitle("Help Systeem")
     .addFields(
-        { name: "📑Algemeen", value: "`!new \n!dog \n!meme \n!ping \n!help \n!leden`" },
-        { name: "📍Moderatie", value: "`!ban \n!kick \n!lock \n!unlock \n!training \n!close`" },
+        { name: "📑Algemeen", value: "`!new !dog !meme !ping \n!help \n!leden`"},
+        { name: "📍Moderatie", value: "`!ban !kick !lock !unlock \n!training \n!close`" },
     )
-    .setThumbnail("https://cdn.discordapp.com/attachments/758699445356462110/769845913203769364/unknown.png")
-    .setFooter("RTD | Rotterdam", "https://cdn.discordapp.com/attachments/758699445356462110/769845913203769364/unknown.png");
+    .setThumbnail("https://cdn.discordapp.com/attachments/758699445356462110/772441369292570624/unknown.png")
+    .setFooter("RTD | Rotterdam", "https://cdn.discordapp.com/attachments/758699445356462110/772441369292570624/unknown.png");
     return message.channel.send(embed);
 }
 
